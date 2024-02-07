5745886653084a1a352886f18b0565e3d4e01909
Екатерина Кудряшова <katyushka555@yandex.ru>
2024-02-07 19:59:37 +0400

    Homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers_data
+(
+	customer_id varchar(20) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(20) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(20) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,39 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+
+def executing_sql(query):
+    """Функция для подключения к БД и выполнения запросов"""
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='02121995')
+    try:
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+    finally:
+        conn.close()
+
+
+def filling_in_tables(table_name, file_csv):
+    """Функция для заполнения таблиц"""
+    with open(file_csv, 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            values = "', '".join(row)
+            query = f"INSERT INTO {table_name} VALUES ('{values}');"
+            executing_sql(query)
+
+
+# Заполнение таблицы employees данными
+employees_csv = 'north_data/employees_data.csv'
+filling_in_tables('employees_data', employees_csv)
+
+# Заполнение таблицы customers данными
+customers_csv = 'north_data/customers_data.csv'
+filling_in_tables('customers_data', customers_csv)
+
+# Заполнение таблицы orders данными
+orders_csv = 'north_data/orders_data.csv'
+filling_in_tables('orders_data', orders_csv)
